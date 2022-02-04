763ef7f648cc8b925119599704962aae3d22f898
Wallace <wallystudyhard@gmail.com>
2022-02-04 12:31:25 -0300

    Firt commit

---
+++ CRUD/RVRentAgencyDB.sql
@@ -0,0 +1,53 @@
+-- Create database
+create database RVRentAgencyDB;
+-- Stsrt DB
+USE RVRentAgencyDB
+
+--============================================================
+
+-- Create the tables--
+
+CREATE TABLE RentRV(
+	RentRVID int IDENTITY(1,1) primary key NOT NULL,
+	FindRV varchar(max) NOT NULL,
+	Price decimal(18, 0) NOT NULL
+	);
+
+CREATE TABLE BuyRV(
+	BuyRVID int IDENTITY(1,1) primary key NOT NULL,
+	FindRV varchar(max) NOT NULL,
+	Price decimal (18, 0) NOT NULL,
+);
+
+
+CREATE TABLE ContactUs(
+	ContactID int IDENTITY(1,1) primary key NOT NULL,
+	Email varchar(max) NOT NULL,
+	NewMessage varchar(max) NOT NULL,
+	);
+
+CREATE TABLE PlanTrip(
+	Plan_tripID int IDENTITY(1,1) primary key NOT NULL,
+	Beginning_from varchar(max) NOT NULL,
+	Arriving_at varchar(max) NOT NULL,
+	fk_BuyRVID int NULL,
+	fk_RentRVID int NULL,
+	);
+
+--============================================================
+
+-- Modify the tables
+alter table PlanTrip add constraint FK_RentRVID
+foreign key (fk_RentRVID)
+references RentRV (RentRVID)
+on delete cascade;
+
+alter table PlanTrip add constraint FK_BuyRVID
+foreign key (fk_BuyRVID)
+references BuyRV (BuyRVID)
+on delete cascade;
+
+alter table PlanTrip add constraint FK_ContactID
+foreign key (fk_ContactID)
+references ContactUs (ContactID)
+on delete cascade;
